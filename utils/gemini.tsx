cad0727938571e49247fb04873f5f499cc7e38f2
Myles Gould <myles.gould.2024@gmail.com>
2024-12-31 03:02:56 -0500

    testing

--- utils/gemini.tsx
+++ utils/gemini.tsx
@@ -8,6 +8,6 @@ const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
 const prompt = "Explain how AI works";
-
+const x = 3
 const result = await model.generateContent(prompt);
 console.log(result.response.text());
